4b549c996b8f0ce6762ee0d924c8e13dc26346cb
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-03 18:08:23 -1000

    another garbage commit?

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../obstools/'))
-sys.path.insert(0, os.path.abspath('../obstools/atacr/'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
