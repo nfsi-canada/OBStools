022d2ed31f259b45cfe54cde1b21f5a3dd972e54
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-03 14:39:26 -1000

    new tests for coverage

---
+++ obstools/tests/test_0_imports.py
@@ -0,0 +1,15 @@
+def test_stdb_import():
+    import stdb
+
+def test_obspy_import():
+    import obspy
+
+def test_obstools_modules():
+    import obstools
+    import obstools.atacr
+    from obstools.atacr import classes
+    from obstools.atacr import utils
+    import matplotlib
+    matplotlib.use('Agg')
+    from obstools.atacr import plot
+    from obstools.atacr.classes import DayNoise, StaNoise, TFNoise, EventStream, Power, Cross, Rotation 
