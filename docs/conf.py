a2b4053ec1d88d50d22e5a8de7f96836359ef815
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-03 17:27:35 -1000

    another try...

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../obstools'))
+sys.path.insert(0, os.path.abspath('../obstools/'))
+sys.path.insert(0, os.path.abspath('../obstools/atacr/'))
 
 
 # -- Project information -----------------------------------------------------
