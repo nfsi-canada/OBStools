4bcfae9965ff97394724eb69e3fe82a4b6fddd10
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-14 20:23:19 -1000

    PEP8 __init__

--- obstools/atacr/__init__.py
+++ obstools/atacr/__init__.py
@@ -9,8 +9,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -20,4 +20,5 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from .classes import DayNoise, StaNoise, TFNoise, EventStream, Power, Cross, Rotation
+from .classes import DayNoise, StaNoise, TFNoise, EventStream
+from .classes import Power, Cross, Rotation
