f0e05521f9b5813cb5761ebf97286654107886fc
Pascal Audet <pascal.audet@uottawa.ca>
2022-02-09 14:17:18 -0500

    fixed parser error tests

--- obstools/tests/test_1_args.py
+++ obstools/tests/test_1_args.py
@@ -2,6 +2,7 @@ import numpy as np
 from pkg_resources import resource_filename
 from pathlib import Path
 import pytest
+from parser import ParserError
 
 
 dbfile = resource_filename('obstools',
@@ -12,7 +13,7 @@ def test_get_daylong_arguments():
     from obstools.scripts import atacr_download_data as atacr
     # no stdb
     with pytest.raises(SystemExit):
-        assert atacr.get_daylong_arguments()
+        atacr.get_daylong_arguments([])
     # defaults
     args0 = atacr.get_daylong_arguments([
         dbfile, '--keys', '7D.MM08',
@@ -24,37 +25,37 @@ def test_get_daylong_arguments():
     args = atacr.get_daylong_arguments([
         dbfile, '--channels', 'H,P'])
     with pytest.raises(SystemExit):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '--channels', 'J,P'])
     # start time
     args = atacr.get_daylong_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = atacr.get_daylong_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '--end', 'abcd'])
     # user auth
     args = atacr.get_daylong_arguments([
         dbfile, '-U', 'user:name'])
     with pytest.raises(SystemExit):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '-U', 'abcd'])
     # sampling rate
     with pytest.raises(SystemExit):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '--sampling-rate', 'abcd'])
     # units
     with pytest.raises(Exception):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '--units', 'abcd'])
     # pre-filt
     with pytest.raises(Exception):
-        assert atacr.get_daylong_arguments([
+        atacr.get_daylong_arguments([
             dbfile, '--pre-filt', '0.1,0.2,0.3'])
 
     return args0
@@ -64,7 +65,7 @@ def test_get_event_arguments():
     from obstools.scripts import atacr_download_event as atacr
     # no stdb
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments()
+        atacr.get_event_arguments([])
     # defaults
     args0 = atacr.get_event_arguments([
         dbfile, '--keys', '7D.MM08',
@@ -77,47 +78,47 @@ def test_get_event_arguments():
     args = atacr.get_event_arguments([
         dbfile, '--channels', 'H,P'])
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--channels', 'J,P'])
     # start time
     args = atacr.get_event_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = atacr.get_event_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--end', 'abcd'])
      # user auth
     args = atacr.get_event_arguments([
         dbfile, '-U', 'user:name'])
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '-U', 'abcd'])
     # sampling rate
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--sampling-rate', 'abcd'])
     # magnitudes
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--min-mag', 'abcd'])
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--max-mag', 'abcd'])
     # distances
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--min-dist', 'abcd'])
     with pytest.raises(SystemExit):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--max-dist', 'abcd'])
     # pre-filt
     with pytest.raises(Exception):
-        assert atacr.get_event_arguments([
+        atacr.get_event_arguments([
             dbfile, '--pre-filt', '0.1,0.2,0.3'])
     return args0
 
@@ -126,7 +127,7 @@ def test_get_dailyspec_arguments():
     from obstools.scripts import atacr_daily_spectra as atacr
     # no stdb
     with pytest.raises(SystemExit):
-        assert atacr.get_dailyspec_arguments()
+        atacr.get_dailyspec_arguments([])
     # defaults
     args0 = atacr.get_dailyspec_arguments([dbfile])
     # keys
@@ -139,19 +140,19 @@ def test_get_dailyspec_arguments():
     args = atacr.get_dailyspec_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_dailyspec_arguments([
+        atacr.get_dailyspec_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = atacr.get_dailyspec_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_dailyspec_arguments([
+        atacr.get_dailyspec_arguments([
             dbfile, '--end', 'abcd'])
     # frequencies
     args = atacr.get_dailyspec_arguments([
         dbfile, '--freq-band', '0.004,2.'])
     with pytest.raises(Exception):
-        assert atacr.get_dailyspec_arguments([
+        atacr.get_dailyspec_arguments([
             dbfile, '--freq-band', '0.1'])
 
     return args0
@@ -161,7 +162,7 @@ def test_get_cleanspec_arguments():
     from obstools.scripts import atacr_clean_spectra as atacr
     # no stdb
     with pytest.raises(SystemExit):
-        assert atacr.get_cleanspec_arguments()
+        atacr.get_cleanspec_arguments([])
     # defaults
     args0 = atacr.get_cleanspec_arguments([dbfile])
     # keys
@@ -171,19 +172,19 @@ def test_get_cleanspec_arguments():
     args = atacr.get_cleanspec_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_cleanspec_arguments([
+        atacr.get_cleanspec_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = atacr.get_cleanspec_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_cleanspec_arguments([
+        atacr.get_cleanspec_arguments([
             dbfile, '--end', 'abcd'])
     # frequencies
     args = atacr.get_cleanspec_arguments([
         dbfile, '--freq-band', '0.004,2.'])
     with pytest.raises(Exception):
-        assert atacr.get_cleanspec_arguments([
+        atacr.get_cleanspec_arguments([
             dbfile, '--freq-band', '0.1'])
     return args0
 
@@ -192,7 +193,7 @@ def test_get_transfer_arguments():
     from obstools.scripts import atacr_transfer_functions as atacr
     # no stdb
     with pytest.raises(SystemExit):
-        assert atacr.get_transfer_arguments()
+        atacr.get_transfer_arguments([])
     # defaults
     args0 = atacr.get_transfer_arguments([dbfile])
     # keys
@@ -202,17 +203,17 @@ def test_get_transfer_arguments():
     args = atacr.get_transfer_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_transfer_arguments([
+        atacr.get_transfer_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = atacr.get_transfer_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_transfer_arguments([
+        atacr.get_transfer_arguments([
             dbfile, '--end', 'abcd'])
     # skip clean
     with pytest.raises(SystemExit):
-        assert atacr.get_transfer_arguments([
+        atacr.get_transfer_arguments([
             dbfile, '--skip-clean', '--skip-daily'])
     return args0
 
@@ -221,7 +222,7 @@ def test_get_correct_arguments():
     from obstools.scripts import atacr_correct_event as atacr
     # no stdb
     with pytest.raises(SystemExit):
-        assert atacr.get_correct_arguments()
+        atacr.get_correct_arguments([])
     # defaults
     args0 = atacr.get_correct_arguments([dbfile])
     # keys
@@ -231,17 +232,17 @@ def test_get_correct_arguments():
     args = atacr.get_correct_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_correct_arguments([
+        atacr.get_correct_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = atacr.get_correct_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert atacr.get_correct_arguments([
+        atacr.get_correct_arguments([
             dbfile, '--end', 'abcd'])
     # skip clean
     with pytest.raises(SystemExit):
-        assert atacr.get_correct_arguments([
+        atacr.get_correct_arguments([
             dbfile, '--skip-clean', '--skip-daily'])
     return args0
 
@@ -250,7 +251,7 @@ def test_get_comply_arguments():
     from obstools.scripts import comply_calculate as comply
     # no stdb
     with pytest.raises(SystemExit):
-        assert comply.get_comply_arguments()
+        comply.get_comply_arguments([])
     # defaults
     args0 = comply.get_comply_arguments([dbfile])
     # keys
@@ -260,21 +261,21 @@ def test_get_comply_arguments():
     args = comply.get_comply_arguments([
         dbfile, '--start', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert comply.get_comply_arguments([
+        comply.get_comply_arguments([
             dbfile, '--start', 'abcd'])
     # end time
     args = comply.get_comply_arguments([
         dbfile, '--end', '2020-01-01'])
     with pytest.raises(SystemExit):
-        assert comply.get_comply_arguments([
+        comply.get_comply_arguments([
             dbfile, '--end', 'abcd'])
     # skip clean
     with pytest.raises(SystemExit):
-        assert comply.get_comply_arguments([
+        comply.get_comply_arguments([
             dbfile, '--skip-clean', '--skip-daily'])
     # save format
     with pytest.raises(SystemExit):
-        assert comply.get_comply_arguments([
+        comply.get_comply_arguments([
             dbfile, '--save-format', 'abcd'])
     return args0
 
