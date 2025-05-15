2178e751f4b056b2e1d3d04c0a090a9b1585235f
Pascal Audet <pascal.audet@uottawa.ca>
2025-05-15 12:11:24 +1200

    add SDS documentation in html docs

--- obstools/__init__.py
+++ obstools/__init__.py
@@ -24,7 +24,7 @@
 Licence
 -------
 
-Copyright 2019 Pascal Audet & Helen Janiszewski
+Copyright 2019 Pascal Audet
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -50,19 +50,14 @@ Installation
 Dependencies
 ++++++++++++
 
-The current version has been tested using **Python 3.10** \
-The following package is required before install ``obstools``:
-
-- `stdb <https://github.com/paudetseis/StDb>`_
-
-Other required packages (e.g., ``obspy``)
-will be automatically installed by ``stdb``.
+- `stdb <https://github.com/schaefferaj/StDb>`_
+- `obspy <https://github.com/obspy/obspy>`_
 
 Conda environment
 +++++++++++++++++
 
 We recommend creating a custom ``conda`` environment
-where ``OBStools`` can be installed along with some of its dependencies.
+where ``OBStools`` can be installed along with its dependencies.
 
 .. sourcecode:: bash
 
@@ -78,14 +73,14 @@ Install dependency:
 
 .. sourcecode:: bash
 
-   pip install stdb
+   pip install git+https://github.com/schaefferaj/stdb
 
 Installing development branch on GitHub
 +++++++++++++++++++++++++++++++++++++++
 
 .. sourcecode:: bash
 
-   pip install obstools@git+https://github.com/nfsi-canada/obstools
+   pip install git+https://github.com/nfsi-canada/obstools
 
 Installing from source
 ++++++++++++++++++++++
@@ -103,11 +98,75 @@ Installing from source
 
    pip install .
 
+Using local data
+================
+
+The various scripts packaged with ``OrientPy`` use FDSN web services
+through and ``ObsPy`` `Client` to load waveform data. For waveform
+data locally stored on your hard drive, the scripts can use a `Client`
+that reads a `SeisComP Data Structure 
+<https://docs.obspy.org/packages/autogen/obspy.clients.filesystem.sds.html>`_ 
+archive containing SAC or miniSEED waveform data. Check out the scripts 
+``bng_calc`` and ``dl_calc`` below and the argument ``--local-data`` and 
+``--dtype`` for more details.
+
+Station Metadata
+----------------
+
+If you have data stored locally on your drive, it is likely you also
+have a station `XML <https://www.fdsn.org/xml/station/>`_ file
+containing the metadata. The corresponding ObsPy documentation is
+`here <https://docs.obspy.org/packages/obspy.core.inventory.html>`_. 
+
+To convert the station `XML` file to an input that can be read by
+``OrientPy``, you run the command ``gen_stdb station.xml`` (only
+available on StDb version 0.2.7), which will create the file
+``station.pkl``. If you don't have a station `XML` file but you have
+a dataless SEED file, you can convert it first to `XML` using `this
+tools <https://seiscode.iris.washington.edu/projects/stationxml-converter>`_.
+
+Waveform Data
+-------------
+
+The SDS folder containing the waveform data has the structure:
+
+.. code-block:: python
+
+   archive
+     + year
+       + network code
+         + station code
+           + channel code + type
+             + one file per day and location, e.g. NET.STA.LOC.CHAN.TYPE.YEAR.DOY
+
+
+For example:
+
+.. code-block:: python
+
+   SDS/
+     2014/
+       YH/
+         LOBS3/
+           HH1.D/ 
+             YH.LOBS3..CH1.D.2014.332
+             ...
+
+
+Note, the filename does not include the extension (`.MSEED` or `.SAC`),
+and the characters `.D` (for type Data) that appear in both the
+channel code and the filename. Note also the two dots (`..`). If
+there is a location code, it should appear between those dots (e.g.,
+for a location code `10`, the corresponding filename should be
+`YH.LOBS3.10.HH1.D.2014.332`). There is no location code for the 
+YH.LOBS3 data, and this field is simply absent from the filenames.
+Finally, the day-of-year (DOY) field must be zero-padded to be exactly
+3 characters.
 """
 
 __version__ = '0.2.1'
 
-__author__ = 'Pascal Audet & Helen Janiszewski'
+__author__ = 'Pascal Audet'
 
 from . import atacr
 from . import comply
